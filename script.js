c6af23a7acde1414b9c26d3974863a4788f1bfbd
owwkay <s4171044@student.rmit.edu.au>
2026-06-18 20:28:16 +1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*let imgElement = document.querySelector("img");
+
+imgElement.addEventListener("mouseover", () => {
+    imgElement.classList.add("flip");
+})
+
+imgElement.addEventListener("mouseout", () => {
+    imgElement.classList.remove("flip");
+})*/
